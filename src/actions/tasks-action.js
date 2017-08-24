2492b9812b147c5ce0833e515a9a93753d5a0460
pmg1989 <972401854@qq.com>
2017-08-24 16:40:55 +0800

    fix tasks-action

--- src/actions/tasks-action.js
+++ src/actions/tasks-action.js
@@ -1,14 +1,18 @@
-import { createAction } from 'redux-actions'
+import { createActions } from 'redux-actions'
 import {TASK_ADD, TASK_DONE, TASK_UNDONE, TASK_REMOVE, TASK_EDIT, TASK_FILTER} from '../constants'
 
-export const taskAdd = createAction(TASK_ADD)
-
-export const taskDone = createAction(TASK_DONE)
-
-export const taskUndone = createAction(TASK_UNDONE)
-
-export const taskRemove = createAction(TASK_REMOVE)
-
-export const taskEdit = createAction(TASK_EDIT)
-
-export const taskFilter = createAction(TASK_FILTER)
+export const {
+  taskAdd,
+  taskDone,
+  taskUndone,
+  taskRemove,
+  taskEdit,
+  taskFilter
+} = createActions({
+  [TASK_ADD]: name => name,
+  [TASK_DONE]: id => id,
+  [TASK_UNDONE]: id => id,
+  [TASK_REMOVE]: id => id,
+  [TASK_EDIT]: ({id, name}) => ({id, name}),
+  [TASK_FILTER]: filter => filter
+})
